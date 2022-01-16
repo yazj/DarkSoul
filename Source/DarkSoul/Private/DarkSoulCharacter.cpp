26e3fd0b8bc909144d28dfd3c302bbfec9d3a1e0
yazj <yuanyi2000@protonmail.com>
2022-01-16 23:24:09 +0800

    Base Code

--- Source/DarkSoul/Private/DarkSoulCharacter.cpp
+++ Source/DarkSoul/Private/DarkSoulCharacter.cpp
@@ -1,34 +1,300 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Yazj, All Rights Reversed
 
 
 #include "DarkSoulCharacter.h"
+#include "Items/DarkSoulItem.h"
+#include "AbilitySystemGlobals.h"
+#include "Abilities/DarkSoulGameplayAbility.h"
 
 // Sets default values
 ADarkSoulCharacter::ADarkSoulCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	// Create ability system component, and set it to be explicitly replicated
+	AbilitySystemComponent = CreateDefaultSubobject<UDarkSoulAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+
+	// Create the attribute set, this replicates by default
+	AttributeSet = CreateDefaultSubobject<UDarkSoulAttributeSet>(TEXT("AttributeSet"));
+
+	CharacterLevel = 1;
+	bAbilitiesInitialized = false;
 
 }
 
-// Called when the game starts or when spawned
-void ADarkSoulCharacter::BeginPlay()
+void ADarkSoulCharacter::PossessedBy(AController* NewController)
 {
-	Super::BeginPlay();
-	
+	Super::PossessedBy(NewController);
+
+	// Try setting the inventory source, this will fail for AI
+	InventorySource = NewController;
+
+	if (InventorySource)
+	{
+		InventoryUpdateHandle = InventorySource->GetSlottedItemChangedDelegate().AddUObject(this, &ADarkSoulCharacter::OnItemSlotChanged);
+		InventoryLoadedHandle = InventorySource->GetInventoryLoadedDelegate().AddUObject(this, &ADarkSoulCharacter::RefreshSlottedGameplayAbilities);
+	}
+
+	// Initialize abilities
+	if (AbilitySystemComponent)
+	{
+		AbilitySystemComponent->InitAbilityActorInfo(this, this);
+		AddStartupGameplayAbilities();
+	}
+
 }
 
-// Called every frame
-void ADarkSoulCharacter::Tick(float DeltaTime)
+void ADarkSoulCharacter::UnPossessed()
 {
-	Super::Tick(DeltaTime);
+	// Unmap for inventory source
+	if (InventorySource && InventoryUpdateHandle.IsValid())
+	{
+		InventorySource->GetSlottedItemChangedDelegate().Remove(InventoryUpdateHandle);
+		InventoryUpdateHandle.Reset();
 
+		InventorySource->GetInventoryLoadedDelegate().Remove(InventoryLoadedHandle);
+		InventoryLoadedHandle.Reset();
+	}
+
+	InventorySource = nullptr;
 }
 
-// Called to bind functionality to input
-void ADarkSoulCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void ADarkSoulCharacter::OnRep_Controller()
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	Super::OnRep_Controller();
+
+	// Controller changed, must update ActorInfo on AbilitySystemComponent
+	if (AbilitySystemComponent)
+	{
+		AbilitySystemComponent->RefreshAbilityActorInfo();
+	}
+}
+
+void ADarkSoulCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ADarkSoulCharacter, CharacterLevel);
+}
+
+UAbilitySystemComponent* ADarkSoulCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+float ADarkSoulCharacter::GetHealth() const
+{
+	return 0.0f;
+}
+
+float ADarkSoulCharacter::GetMaxHealth() const
+{
+	return 0.0f;
+}
+
+float ADarkSoulCharacter::GetMana() const
+{
+	return 0.0f;
+}
+
+float ADarkSoulCharacter::GetMaxMana() const
+{
+	return 0.0f;
+}
+
+float ADarkSoulCharacter::GetMoveSpeed() const
+{
+	return 0.0f;
+}
+
+int32 ADarkSoulCharacter::GetCharacterLevel() const
+{
+	return int32();
+}
+
+bool ADarkSoulCharacter::SetCharacterLevel(int32 NewLevel)
+{
+	return false;
+}
+
+bool ADarkSoulCharacter::ActivateAbilitiesWithItemSlot(FDarkSoulItemSlot ItemSlot, bool bAllowRemoteActivation)
+{
+	return false;
+}
+
+void ADarkSoulCharacter::GetActiveAbilitiesWithItemSlot(FDarkSoulItemSlot ItemSlot, TArray<UDarkSoulGameplayAbility*>& ActiveAbilities)
+{
+}
+
+bool ADarkSoulCharacter::ActivateAbilitiesWithTags(FGameplayTagContainer AbilityTags, bool bAllowRemoteActivation)
+{
+	return false;
+}
+
+void ADarkSoulCharacter::GetActiveAbilitiesWithTags(FGameplayTagContainer AbilityTags, TArray<UDarkSoulGameplayAbility*>& ActiveAbilities)
+{
+}
+
+bool ADarkSoulCharacter::GetCooldownRemainingForTag(FGameplayTagContainer CooldownTags, float& TimeRemaining, float& CooldownDuration)
+{
+	return false;
+}
+
+void ADarkSoulCharacter::OnItemSlotChanged(FDarkSoulItemSlot ItemSlot, UDarkSoulItem* Item)
+{
+	RefreshSlottedGameplayAbilities();
+}
+
+void ADarkSoulCharacter::RefreshSlottedGameplayAbilities()
+{
+}
+
+void ADarkSoulCharacter::AddStartupGameplayAbilities()
+{
+	check(AbilitySystemComponent);
+
+	if (GetLocalRole() == ROLE_Authority && !bAbilitiesInitialized)
+	{
+		// Grant abilities, but only on the server
+		for (TSubclassOf<UDarkSoulGameplayAbility>& StartupAbility : GameplayAbilities)
+		{
+			AbilitySystemComponent->GiveAbility(FGameplayAbilitySpec(StartupAbility, GetCharacterLevel(), INDEX_NONE, this));
+		}
+
+		// Now apply passive
+		for (TSubclassOf<UGameplayEffect>& GameplayEffect : PassiveGameplayEffects)
+		{
+			FGameplayEffectContextHandle EffectContext = AbilitySystemComponent->MakeEffectContext();
+			EffectContext.AddSourceObject(this);
+
+			FGameplayEffectSpecHandle NewHandle = AbilitySystemComponent->MakeOutgoingSpec(GameplayEffect, GetCharacterLevel(), EffectContext);
+			if (NewHandle.IsValid())
+			{
+				FActiveGameplayEffectHandle ActiveGEHanle = AbilitySystemComponent->ApplyGameplayEffectSpecToTarget(*NewHandle.Data.Get(), AbilitySystemComponent);
+			}
+		}
+
+	}
+
+	AddSlottedGameplayAbilities();
+
+	bAbilitiesInitialized = true;
 
 }
+
+void ADarkSoulCharacter::RemoveStartupGameplayAbilities()
+{
+	check(AbilitySystemComponent);
+
+	if (GetLocalRole() == ROLE_Authority && bAbilitiesInitialized)
+	{
+		// Remove any abilities added from a previous call
+		TArray<FGameplayAbilitySpecHandle> AbilitiesToRemove;
+		for (const FGameplayAbilitySpec& Spec : AbilitySystemComponent->GetActivatableAbilities())
+		{
+			if ((Spec.SourceObject == this) && GameplayAbilities.Contains(Spec.Ability->GetClass()))
+			{
+				AbilitiesToRemove.Add(Spec.Handle);
+			}
+		}
+
+		// Do in two passes so the removal happens after we have the full list
+		for (int32 i = 0; i < AbilitiesToRemove.Num(); i++)
+		{
+			AbilitySystemComponent->ClearAbility(AbilitiesToRemove[i]);
+		}
+
+		// Remove all of the passive gameplay effects that were applied by this character
+		FGameplayEffectQuery Query;
+		Query.EffectSource = this;
+		AbilitySystemComponent->RemoveActiveEffects(Query);
+
+		RemoveSlottedGameplayAbilities(true);
+
+		bAbilitiesInitialized = false;
+	}
+}
+
+void ADarkSoulCharacter::AddSlottedGameplayAbilities()
+{
+	TMap<FDarkSoulItemSlot, FGameplayAbilitySpec> SlottedAbilitySpecs;
+	FillSlottedAbilitySpecs(SlottedAbilitySpecs);
+
+	// Now add abilities if needed
+	for (const TPair<FDarkSoulItemSlot, FGameplayAbilitySpec>& SpecPair : SlottedAbilitySpecs)
+	{
+		FGameplayAbilitySpecHandle& SpecHandle = SlottedAbilities.FindOrAdd(SpecPair.Key);
+
+		if (!SpecHandle.IsValid())
+		{
+			SpecHandle = AbilitySystemComponent->GiveAbility(SpecPair.Value);
+		}
+	}
+
+}
+
+void ADarkSoulCharacter::FillSlottedAbilitySpecs(TMap<FDarkSoulItemSlot, FGameplayAbilitySpec>& SlottedAbilitySpecs)
+{
+}
+
+void ADarkSoulCharacter::RemoveSlottedGameplayAbilities(bool bRemoveAll)
+{
+	TMap<FDarkSoulItemSlot, FGameplayAbilitySpec> SlottedAbilitySpecs;
+
+	if (!bRemoveAll)
+	{
+		// Fill in map so we can compare
+		FillSlottedAbilitySpecs(SlottedAbilitySpecs);
+	}
+
+	for (TPair<FDarkSoulItemSlot, FGameplayAbilitySpecHandle>& ExistingPair : SlottedAbilities)
+	{
+		FGameplayAbilitySpec* FoundSpec = AbilitySystemComponent->FindAbilitySpecFromHandle(ExistingPair.Value);
+		bool bShouldRemove = bRemoveAll || !FoundSpec;
+
+		if (!bShouldRemove)
+		{
+			// Need to check desired ability specs, if we got here FoundSpec is valid
+			FGameplayAbilitySpec* DesiredSpec = SlottedAbilitySpecs.Find(ExistingPair.Key);
+
+			if (!DesiredSpec || DesiredSpec->Ability != FoundSpec->Ability || DesiredSpec->SourceObject != FoundSpec->SourceObject)
+			{
+				bShouldRemove = true;
+			}
+
+		}
+
+		if (bShouldRemove)
+		{
+			if (FoundSpec)
+			{
+				// Need to remove registered ability
+				AbilitySystemComponent->ClearAbility(ExistingPair.Value);
+			}
+			// Make sure handle is cleared even if ability wasn't found
+			ExistingPair.Value = FGameplayAbilitySpecHandle();
+		}
+	}
+}
+
+void ADarkSoulCharacter::HandleDamage(float DamageAmount, const FHitResult& HitInfo, const FGameplayTagContainer& DamageTags, ADarkSoulCharacter* InstigatorCharacter, AActor* DamageCauser)
+{
+}
+
+void ADarkSoulCharacter::HandleHealthChanged(float DeltaValue, const FGameplayTagContainer& EventTags)
+{
+}
+
+void ADarkSoulCharacter::HandleManaChanged(float DeltaValue, const FGameplayTagContainer& EventTags)
+{
+}
+
+void ADarkSoulCharacter::HandleMoveSpeedChanged(float DeltaValue, const FGameplayTagContainer& EventTags)
+{
+}
+
+FGenericTeamId ADarkSoulCharacter::GetGenericTeamId() const
+{
+	return FGenericTeamId();
+}
+
 
--- Source/DarkSoul/Public/DarkSoulCharacter.h
+++ Source/DarkSoul/Public/DarkSoulCharacter.h
@@ -1,29 +1,217 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Yazj, All Rights Reversed
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "DarkSoul.h"
 #include "GameFramework/Character.h"
+#include "UObject/ScriptInterface.h"
+#include "DarkSoulInventoryInterface.h"
+#include "AbilitySystemInterface.h"
+#include "Abilities/DarkSoulAbilitySystemComponent.h"
+#include "Abilities/DarkSoulAttributeSet.h"
+#include "GenericTeamAgentInterface.h"
 #include "DarkSoulCharacter.generated.h"
 
+class UDarkSoulGameplayAbility;
+class UGameplayEffect;
+
+/** Base class for character, Designed to be blueprinted */
 UCLASS()
-class DARKSOUL_API ADarkSoulCharacter : public ACharacter
+class DARKSOUL_API ADarkSoulCharacter : public ACharacter, public IAbilitySystemInterface, public IGenericTeamAgentInterface
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
+	// Constructor and overrides
 	ADarkSoulCharacter();
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void UnPossessed() override;
+	virtual void OnRep_Controller() override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	// Implement IAbilitySystemInterface
+	UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	/** Return current health, will be 0 if dead */
+	UFUNCTION(BlueprintCallable)
+	virtual float GetHealth() const;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	/** Returns maximum health, health will never be greater than this */
+	UFUNCTION(BlueprintCallable)
+	virtual float GetMaxHealth() const;
+
+	/** Returns current mana */
+	UFUNCTION(BlueprintCallable)
+	virtual float GetMana() const;
+
+	/** Returns maximum mana, mana will never be greater than this */
+	UFUNCTION(BlueprintCallable)
+	virtual float GetMaxMana() const;
+
+	/** Returns current movement speed */
+	UFUNCTION(BlueprintCallable)
+	virtual float GetMoveSpeed() const;
+
+	/** Returns the character level that is passed to the ability system */
+	UFUNCTION(BlueprintCallable)
+	virtual int32 GetCharacterLevel() const;
+
+	/** Modifies the character level, this may change abilities. Returns true on success */
+	UFUNCTION(BlueprintCallable)
+	virtual bool SetCharacterLevel(int32 NewLevel);
+
+	/**
+	*	Attempts to activate any ability in the specified item slot. Will return false if no activatable ability found or activation fails
+	*   Returns true if it thinks it activated, but it may return false positive due to failure later in activation
+	*	If bAllowRemoteActivation is true, it will remotely activate local/server abilities,
+	*	otherwise it will only try to locally activate the ability
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Abilities")
+	bool ActivateAbilitiesWithItemSlot(FDarkSoulItemSlot ItemSlot, bool bAllowRemoteActivation = true);
+	
+	/** Returns a list of active abilities bound to the item slot. This only returns if the ability is current running */
+	UFUNCTION(BlueprintCallable, Category = "Abilities")
+	void GetActiveAbilitiesWithItemSlot(FDarkSoulItemSlot ItemSlot,
+		TArray<UDarkSoulGameplayAbility*>& ActiveAbilities);
+
+	/**
+	*	Attempts to active all abilities that match the specified tags
+	*	Returns true if it thinks it activated, but it may return false positive due to failure later in activation.
+	*	If bAllowRemoteActivation is true, it will remotely activate local/server abilities,
+	*	otherwise it will only try to locally activate the ability
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Abilities")
+	bool ActivateAbilitiesWithTags(FGameplayTagContainer AbilityTags, bool bAllowRemoteActivation = true);
+
+	/** Returns a list of active abilities matching the specified tags. This only returns if the ability is current running */
+	UFUNCTION(BlueprintCallable, Category = "Abilities")
+	void GetActiveAbilitiesWithTags(FGameplayTagContainer AbilityTags,
+		TArray<UDarkSoulGameplayAbility*>& ActiveAbilities);
+
+	/** Returns total time and remaining time for cooldown tags. Return false if no active cooldown found */
+	UFUNCTION(BlueprintCallable, Category = "Abilities")
+	bool GetCooldownRemainingForTag(FGameplayTagContainer CooldownTags,
+		float& TimeRemaining,
+		float& CooldownDuration);
+
+protected:	
+	/** The level of this character, should not be modified directly once it has already spawned */
+	UPROPERTY(EditAnywhere, Replicated, Category = Abilities)
+	int32 CharacterLevel;
+
+	/** Abilities to grant to this character on creation.
+	*	This will be activated by tag or event and are not bound to specific inputs
+	*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Abilities)
+	TArray<TSubclassOf<UDarkSoulGameplayAbility>> GameplayAbilities;
+
+	/** Map of item slot to gameplay ability class, these are bound any abilities added by the inventory  */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Abilities)
+	TMap<FDarkSoulItemSlot, TSubclassOf<UDarkSoulGameplayAbility>> DefaultSlottedAbilities;
+
+	/** Passive gameplay effects applied on creation */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Abilities)
+	TArray<TSubclassOf<UGameplayEffect>> PassiveGameplayEffects;
+
+	/** The component used to handle ability system interactions */
+	UPROPERTY()
+	UDarkSoulAbilitySystemComponent* AbilitySystemComponent;
+
+	/** List of attributes modified by the ability system */
+	UPROPERTY()
+	UDarkSoulAttributeSet* AttributeSet;
+
+	/** Cached pointer to the inventory source for this character, can be null */
+	UPROPERTY()
+	TScriptInterface<IDarkSoulInventoryInterface> InventorySource;
+
+	/** If true we have initialized our abilities */
+	UPROPERTY()
+	int32 bAbilitiesInitialized;
+
+	/** Map of slot to ability granted by the slot */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Inventory)
+	TMap<FDarkSoulItemSlot, FGameplayAbilitySpecHandle> SlottedAbilities;
+
+	/** Delegate handles */
+	FDelegateHandle InventoryUpdateHandle;
+	FDelegateHandle InventoryLoadedHandle;
+
+	/**
+	*	Called when character takes damage, which may have killed them
+	* 
+	*	@param DamageAmount Amount of damage that was done, not clamped based on current health
+	*	@param HitInfo The hit info that generated this damage
+	*	@param DamageTags the gameplay tags of the event that did the damage
+	*	@param InstigatorCharacter the character that initiated this damage
+	*	@param DamageCauser The actual actor that did the damage, might be a weapon or projectile
+	*/
+	UFUNCTION(BlueprintimplementableEvent)
+	void OnDamaged(float DamageAmount, const FHitResult& HitInfo,
+		const struct FGameplayTagContainer& DamageTags, ADarkSoulCharacter* DamageCauser);
+
+	/**
+	*	Called when health is changed, either from healing or from being damaged
+	*	For damage this is called in addition to OnDamaged/OnKilled
+	* 
+	*	@param DeltaValue Change in health value, positive for heal, negative for cost.If 0 the delta is unknown
+	*	@param EventTags the gameplay tags of the event that changed health
+	*/
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnHealthChanged(float DeltaValue, const struct FGameplayTagContainer& EventTags);
+
+	/**
+	*	Called when mana is changed, either from healing or from being damaged
+	*
+	*	@param DeltaValue Change in health value, positive for heal, negative for cost.If 0 the delta is unknown
+	*	@param EventTags the gameplay tags of the event that changed mana
+	*/
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnManaChanged(float DeltaValue, const struct FGameplayTagContainer& EventTags);
+
+	/**
+	*	Called when movement speed is changed
+	* 
+	*	@param DeltaValue Change in move speed
+	*	@param EventTags The gameplay tags of the event that changed mana
+	*/
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnMoveSpeedChanged(float DeltaValue, const struct FGameplayTagContainer& EventTags);
+
+	/** Called when slotted items change, bound to delegate on interface */
+	void OnItemSlotChanged(FDarkSoulItemSlot ItemSlot, UDarkSoulItem* Item);
+	void RefreshSlottedGameplayAbilities();
+
+	/** Apply the startup gameplay abilities and effects */
+	void AddStartupGameplayAbilities();
+
+	/** Attempts to remove any startup gameplay abilities */
+	void RemoveStartupGameplayAbilities();
+
+	/** Adds slotted item abilities if needed */
+	void AddSlottedGameplayAbilities();
+
+	/** Fills in with ability specs, based on defaults and inventory */
+	void FillSlottedAbilitySpecs(TMap<FDarkSoulItemSlot, FGameplayAbilitySpec>& SlottedAbilitySpecs);
+
+	/** Remove slotted gameplay abilities, if force is false it only removes invalid ones */
+	void RemoveSlottedGameplayAbilities(bool bRemoveAll);
+
+	// Called from DarkSoulAttributeSet, these call BP events above
+	virtual void HandleDamage(
+		float DamageAmount,
+		const FHitResult& HitInfo,
+		const struct FGameplayTagContainer& DamageTags,
+		ADarkSoulCharacter* InstigatorCharacter,
+		AActor* DamageCauser);
+	virtual void HandleHealthChanged(float DeltaValue, const struct FGameplayTagContainer& EventTags);
+	virtual void HandleManaChanged(float DeltaValue, const struct FGameplayTagContainer& EventTags);
+	virtual void HandleMoveSpeedChanged(float DeltaValue, const struct FGameplayTagContainer& EventTags);
+
+	/** Required to support AIPerceptionSystem */
+	virtual FGenericTeamId GetGenericTeamId() const override;
+
+	// Friended to allow access to handle functions above
+	friend UDarkSoulAttributeSet;
 
 };
