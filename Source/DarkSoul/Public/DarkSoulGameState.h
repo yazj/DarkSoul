75a00d08bddfd97d4dae3c289446f32979e4338a
yazj <yuanyi2000@protonmail.com>
2022-01-16 11:28:38 +0800

    Base Code

---
+++ Source/DarkSoul/Public/DarkSoulGameState.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "DarkSoul.h"
+#include "GameFramework/GameStateBase.h"
+#include "DarkSoulGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DARKSOUL_API ADarkSoulGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+
+public:
+	/** Constructor */
+	ADarkSoulGameState() {}
+	
+};
