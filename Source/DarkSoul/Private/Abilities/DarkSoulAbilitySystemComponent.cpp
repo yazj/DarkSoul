b8082289048868cca1183539b4b882f2d804a3c1
yazj <yuanyi2000@protonmail.com>
2022-01-17 22:41:55 +0800

    Base Code

--- Source/DarkSoul/Private/Abilities/DarkSoulAbilitySystemComponent.cpp
+++ Source/DarkSoul/Private/Abilities/DarkSoulAbilitySystemComponent.cpp
@@ -1,5 +1,40 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "AbilitySystemGlobals.h"
+#include "Abilities/DarkSoulGameplayAbility.h"
+#include "DarkSoulCharacter.h"
 #include "Abilities/DarkSoulAbilitySystemComponent.h"
 
+void UDarkSoulAbilitySystemComponent::GetActiveAbilitiesWithTags(const FGameplayTagContainer& GameplayTagContainer, TArray<UDarkSoulGameplayAbility*>& ActiveAbilities)
+{
+	TArray<FGameplayAbilitySpec*> AbilitiesToActivate;
+	GetActivatableGameplayAbilitySpecsByAllMatchingTags(GameplayTagContainer, AbilitiesToActivate, false);
+
+	// Iterate the list of all ability specs
+	for (FGameplayAbilitySpec* Spec : AbilitiesToActivate)
+	{
+		// Iterate all instances on this ability spec
+		TArray<UGameplayAbility*> AbilityInstances = Spec->GetAbilityInstances();
+
+		for (UGameplayAbility* ActiveAbility : AbilityInstances)
+		{
+			ActiveAbilities.Add(Cast<UDarkSoulGameplayAbility>(ActiveAbility));
+		}
+	}
+}
+
+int32 UDarkSoulAbilitySystemComponent::GetDefaultAbilityLevel() const
+{
+	ADarkSoulCharacter* OwningCharacter = Cast<ADarkSoulCharacter>(GetOwnerActor());
+
+	if (OwningCharacter)
+	{
+		return OwningCharacter->GetCharacterLevel();
+	}
+	return 1;
+}
+
+UDarkSoulAbilitySystemComponent* UDarkSoulAbilitySystemComponent::GetAbilitySystemComponentFromActor(const AActor* Actor, bool LookForComponent)
+{
+	return Cast<UDarkSoulAbilitySystemComponent>(UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(Actor, LookForComponent));
+}
--- Source/DarkSoul/Public/Abilities/DarkSoulAbilitySystemComponent.h
+++ Source/DarkSoul/Public/Abilities/DarkSoulAbilitySystemComponent.h
@@ -4,6 +4,7 @@
 
 #include "DarkSoul.h"
 #include "AbilitySystemComponent.h"
+#include "Abilities/DarkSoulGameplayAbility.h"
 #include "Abilities/DarkSoulAbilityTypes.h"
 #include "DarkSoulAbilitySystemComponent.generated.h"
 
@@ -14,5 +15,20 @@ UCLASS()
 class DARKSOUL_API UDarkSoulAbilitySystemComponent : public UAbilitySystemComponent
 {
 	GENERATED_BODY()
+
+public:
+	// Constructors and overrides
+	UDarkSoulAbilitySystemComponent() {}
+
+	/** Returns a list of currently active ability instance that match the tags */
+	void GetActiveAbilitiesWithTags(const FGameplayTagContainer& GameplayTagContainer, TArray<UDarkSoulGameplayAbility*>& ActiveAbilities);
+
+	/** Returns the default level used for ability activations, derived from the character */
+	int32 GetDefaultAbilityLevel() const;
+
+	/** Version of function in AbilitySystemGlobals that returns correct type */
+	static UDarkSoulAbilitySystemComponent* GetAbilitySystemComponentFromActor(const AActor* Actor, bool LookForComponent = false);
+
+	
 	
 };
