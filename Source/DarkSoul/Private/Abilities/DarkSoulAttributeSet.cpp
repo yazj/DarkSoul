439ba13f9baa0255480545a252106741a5110118
yazj <yuanyi2000@protonmail.com>
2022-01-17 23:07:27 +0800

    Base Code

--- Source/DarkSoul/Private/Abilities/DarkSoulAttributeSet.cpp
+++ Source/DarkSoul/Private/Abilities/DarkSoulAttributeSet.cpp
@@ -1,5 +1,79 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// Copyright Yazj, All Rights Reversed
 
 #include "Abilities/DarkSoulAttributeSet.h"
+#include "Abilities/DarkSoulAbilitySystemComponent.h"
+#include "DarkSoulCharacter.h"
+#include "GameplayEffect.h"
+#include "GameplayEffectExtension.h"
 
+UDarkSoulAttributeSet::UDarkSoulAttributeSet()
+	: Health(1.f)
+	, MaxHealth(1.f)
+	, Mana(0.f)
+	, MaxMana(0.f)
+	, AttackPower(1.0f)
+	, DefensePower(1.0f)
+	, MoveSpeed(1.0f)
+	, Damage(0.0f)
+{}
+
+void UDarkSoulAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+}
+
+void UDarkSoulAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+}
+
+void UDarkSoulAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UDarkSoulAttributeSet, Health);
+	DOREPLIFETIME(UDarkSoulAttributeSet, MaxHealth);
+	DOREPLIFETIME(UDarkSoulAttributeSet, Mana);
+	DOREPLIFETIME(UDarkSoulAttributeSet, MaxMana);
+	DOREPLIFETIME(UDarkSoulAttributeSet, AttackPower);
+	DOREPLIFETIME(UDarkSoulAttributeSet, DefensePower);
+	DOREPLIFETIME(UDarkSoulAttributeSet, MoveSpeed);
+}
+
+void UDarkSoulAttributeSet::AdjustAttributeForMaxChange(FGameplayAttributeData& AffectedAttribute, const FGameplayAttributeData& MaxAttribute, float NewMaxValue, const FGameplayAttribute& AffectedAttributeProperty)
+{
+
+}
+
+void UDarkSoulAttributeSet::OnRep_Health(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDarkSoulAttributeSet, Health, OldValue);
+}
+
+void UDarkSoulAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDarkSoulAttributeSet, MaxHealth, OldValue);
+}
+
+void UDarkSoulAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDarkSoulAttributeSet, Mana, OldValue);
+}
+
+void UDarkSoulAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDarkSoulAttributeSet, MaxMana, OldValue);
+}
+
+void UDarkSoulAttributeSet::OnRep_AttackPower(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDarkSoulAttributeSet, AttackPower, OldValue);
+}
+
+void UDarkSoulAttributeSet::OnRep_DefensePower(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDarkSoulAttributeSet, DefensePower, OldValue);
+}
+
+void UDarkSoulAttributeSet::OnRep_MoveSpeed(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UDarkSoulAttributeSet, MoveSpeed, OldValue);
+}
--- Source/DarkSoul/Public/Abilities/DarkSoulAttributeSet.h
+++ Source/DarkSoul/Public/Abilities/DarkSoulAttributeSet.h
@@ -1,10 +1,18 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Yazj, All Rights Reversed
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "DarkSoul.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "DarkSoulAttributeSet.generated.h"
+
+// Uses macros from AttributeSet.h
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -13,5 +21,78 @@ UCLASS()
 class DARKSOUL_API UDarkSoulAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	// Constructor and overrides
+	UDarkSoulAttributeSet();
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	/** Current health, when 0 character is dead. Capped by MaxHealth */
+	UPROPERTY(BlueprintReadOnly, Category = "Health", ReplicatedUsing=OnRep_Health)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UDarkSoulAttributeSet, Health);
+
+	/** MaxHealth is its own attribute, since GameplayEffects may modify it  */
+	UPROPERTY(BlueprintReadOnly, Category = "Health", ReplicatedUsing = OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UDarkSoulAttributeSet, MaxHealth);
+
+	/** Current mana, used to execute special abilities. Capped by MaxMana */
+	UPROPERTY(BlueprintReadOnly, Category = "Mana", ReplicatedUsing = OnRep_Mana)
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UDarkSoulAttributeSet, Mana);
+
+	/** MaxMana is its own attribute, since GameplayEffects may modify it */
+	UPROPERTY(BlueprintReadOnly, Category = "Mana", ReplicatedUsing = OnRep_MaxMana)
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UDarkSoulAttributeSet, MaxMana)
+
+	/** AttackPower of the attacker is multiplied by the base Damage to reduce health, so 1.0 means no bonus */
+	UPROPERTY(BlueprintReadOnly, Category = "Damage", ReplicatedUsing = OnRep_AttackPower)
+	FGameplayAttributeData AttackPower;
+	ATTRIBUTE_ACCESSORS(UDarkSoulAttributeSet, AttackPower)
+
+	/** Base Damage is divided by DefensePower to get actual damage done, so 1.0 means no bonus */
+	UPROPERTY(BlueprintReadOnly, Category = "Damage", ReplicatedUsing = OnRep_DefensePower)
+	FGameplayAttributeData DefensePower;
+	ATTRIBUTE_ACCESSORS(UDarkSoulAttributeSet, DefensePower)
+
+	/** MoveSpeed affects how fast characters can move */
+	UPROPERTY(BlueprintReadOnly, Category = "MoveSpeed", ReplicatedUsing = OnRep_MoveSpeed)
+	FGameplayAttributeData MoveSpeed;
+	ATTRIBUTE_ACCESSORS(UDarkSoulAttributeSet, MoveSpeed)
+
+	/** Damage is a 'temporary' attribute used by the DamageExecution to calculate final damage, which then turns into -Health */
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData Damage;
+	ATTRIBUTE_ACCESSORS(UDarkSoulAttributeSet, Damage)
+
+protected:
+	/** Helper function to proportionally adjust the value of an attribute when it's associated max attribute changes. (i.e. When MaxHealth increases, Health increases by an amount that maintains the same percentage as before) */
+	void AdjustAttributeForMaxChange(FGameplayAttributeData& AffectedAttribute, const FGameplayAttributeData& MaxAttribute, float NewMaxValue, const FGameplayAttribute& AffectedAttributeProperty);
+
+	// These OnRep functions exist to make sure that the ability system internal representations are synchronized properly during replication
+	UFUNCTION()
+	virtual void OnRep_Health(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	virtual void OnRep_MaxHealth(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	virtual void OnRep_Mana(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	virtual void OnRep_MaxMana(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	virtual void OnRep_AttackPower(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	virtual void OnRep_DefensePower(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	virtual void OnRep_MoveSpeed(const FGameplayAttributeData& OldValue);
 	
 };
