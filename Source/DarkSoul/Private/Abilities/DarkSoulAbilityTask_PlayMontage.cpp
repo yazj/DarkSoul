f81b5c83d3c7feb10377b86acee0b6932e5d689b
yazj <yuanyi2000@protonmail.com>
2022-01-18 16:15:53 +0800

    Base Code

--- Source/DarkSoul/Private/Abilities/DarkSoulAbilityTask_PlayMontage.cpp
+++ Source/DarkSoul/Private/Abilities/DarkSoulAbilityTask_PlayMontage.cpp
@@ -1,5 +1,272 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Yazj, All Rights Reversed
 
 
 #include "Abilities/DarkSoulAbilityTask_PlayMontage.h"
+#include "Abilities/DarkSoulAbilitySystemComponent.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+#include "Animation/AnimInstance.h"
 
+UDarkSoulAbilityTask_PlayMontage::UDarkSoulAbilityTask_PlayMontage(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	Rate = 1.f;
+	bStopWhenAbilityEnds = true;
+}
+
+void UDarkSoulAbilityTask_PlayMontage::Activate()
+{
+	if (Ability == nullptr)
+	{
+		return;
+	}
+
+	bool bPlayedMontage = false;
+	UDarkSoulAbilitySystemComponent* DarkSoulAbilitySystemComponent = GetTargetASC();
+
+	if (DarkSoulAbilitySystemComponent)
+	{
+		const FGameplayAbilityActorInfo* ActorInfo = Ability->GetCurrentActorInfo();
+		UAnimInstance* AnimInstance = ActorInfo->GetAnimInstance();
+		if (AnimInstance != nullptr)
+		{
+			// Bind to event callback
+			EventHandle = DarkSoulAbilitySystemComponent->AddGameplayEventTagContainerDelegate(EventTags, FGameplayEventTagMulticastDelegate::FDelegate::CreateUObject(this, &UDarkSoulAbilityTask_PlayMontage::OnGameplayEvent));
+
+			if (DarkSoulAbilitySystemComponent->PlayMontage(Ability, Ability->GetCurrentActivationInfo(), MontageToPlay, Rate, StartSection) > 0.f)
+			{
+				// Playing a montage could potentially fire off a callback into game code which could kill this ability! Early out if we are  pending kill.
+				if (ShouldBroadcastAbilityTaskDelegates() == false)
+				{
+					return;
+				}
+
+				CancelledHandle = Ability->OnGameplayAbilityCancelled.AddUObject(this, &UDarkSoulAbilityTask_PlayMontage::OnAbilityCancelled);
+
+				BlendingOutDelegate.BindUObject(this, &UDarkSoulAbilityTask_PlayMontage::OnMontageBlendingOut);
+				AnimInstance->Montage_SetBlendingOutDelegate(BlendingOutDelegate, MontageToPlay);
+
+				MontageEndedDelegate.BindUObject(this, &UDarkSoulAbilityTask_PlayMontage::OnMontageEnded);
+				AnimInstance->Montage_SetEndDelegate(MontageEndedDelegate, MontageToPlay);
+
+				ACharacter* Character = Cast<ACharacter>(GetAvatarActor());
+				if (Character && (Character->GetLocalRole() == ROLE_Authority ||
+					(Character->GetLocalRole() == ROLE_AutonomousProxy && Ability->GetNetExecutionPolicy() == EGameplayAbilityNetExecutionPolicy::LocalPredicted)))
+				{
+					Character->SetAnimRootMotionTranslationScale(AnimRootMotionTranslationScale);
+				}
+
+				bPlayedMontage = true;
+			}
+		}
+		else
+		{
+			ABILITY_LOG(Warning, TEXT("URPGAbilityTask_PlayMontageAndWaitForEvent call to PlayMontage failed!"));
+		}
+	}
+	else
+	{
+		ABILITY_LOG(Warning, TEXT("URPGAbilityTask_PlayMontageAndWaitForEvent called on invalid AbilitySystemComponent"));
+	}
+
+	if (!bPlayedMontage)
+	{
+		ABILITY_LOG(Warning, TEXT("URPGAbilityTask_PlayMontageAndWaitForEvent called in Ability %s failed to play montage %s; Task Instance Name %s."), *Ability->GetName(), *GetNameSafe(MontageToPlay), *InstanceName.ToString());
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnCancelled.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+
+	SetWaitingOnAvatar();
+}
+
+void UDarkSoulAbilityTask_PlayMontage::ExternalCancel()
+{
+	check(AbilitySystemComponent);
+
+	OnAbilityCancelled();
+
+	Super::ExternalCancel();
+}
+
+FString UDarkSoulAbilityTask_PlayMontage::GetDebugString() const
+{
+	UAnimMontage* PlayingMontage = nullptr;
+	if (Ability)
+	{
+		const FGameplayAbilityActorInfo* ActorInfo = Ability->GetCurrentActorInfo();
+		UAnimInstance* AnimInstance = ActorInfo->GetAnimInstance();
+
+		if (AnimInstance != nullptr)
+		{
+			PlayingMontage = AnimInstance->Montage_IsActive(MontageToPlay) ? MontageToPlay : AnimInstance->GetCurrentActiveMontage();
+		}
+	}
+
+	return FString::Printf(TEXT("PlayMontageAndWaitForEvent. MontageToPlay: %s  (Currently Playing): %s"), *GetNameSafe(MontageToPlay), *GetNameSafe(PlayingMontage));
+}
+
+void UDarkSoulAbilityTask_PlayMontage::OnDestroy(bool AbilityEnded)
+{
+	// Note: Clearing montage end delegate isn't necessary since its not a multicast and will be cleared when the next montage plays.
+	// (If we are destroyed, it will detect this and not do anything)
+
+	// This delegate, however, should be cleared as it is a multicast
+	if (Ability)
+	{
+		Ability->OnGameplayAbilityCancelled.Remove(CancelledHandle);
+		if (AbilityEnded && bStopWhenAbilityEnds)
+		{
+			StopPlayingMontage();
+		}
+	}
+
+	UDarkSoulAbilitySystemComponent* DarkSoulAbilitySystemComponent = GetTargetASC();
+	if (DarkSoulAbilitySystemComponent)
+	{
+		DarkSoulAbilitySystemComponent->RemoveGameplayEventTagContainerDelegate(EventTags, EventHandle);
+	}
+
+	Super::OnDestroy(AbilityEnded);
+}
+
+UDarkSoulAbilityTask_PlayMontage* UDarkSoulAbilityTask_PlayMontage::PlayMontageAndWaitForEvent(
+	UGameplayAbility* OwningAbility, 
+	FName TaskInstanceName, 
+	UAnimMontage* MontageToPlay, 
+	FGameplayTagContainer EventTags, 
+	float Rate, 
+	FName StartSection, 
+	bool bStopWhenAbilityEnds, 
+	float AnimRootMotionTranslationScale
+)
+{
+	UAbilitySystemGlobals::NonShipping_ApplyGlobalAbilityScaler_Rate(Rate);
+
+	UDarkSoulAbilityTask_PlayMontage* MyObj = NewAbilityTask<UDarkSoulAbilityTask_PlayMontage>(OwningAbility, TaskInstanceName);
+	MyObj->MontageToPlay = MontageToPlay;
+	MyObj->EventTags = EventTags;
+	MyObj->Rate = Rate;
+	MyObj->StartSection = StartSection;
+	MyObj->AnimRootMotionTranslationScale = AnimRootMotionTranslationScale;
+	MyObj->bStopWhenAbilityEnds = bStopWhenAbilityEnds;
+
+	return MyObj;
+}
+
+bool UDarkSoulAbilityTask_PlayMontage::StopPlayingMontage()
+{
+	const FGameplayAbilityActorInfo* ActorInfo = Ability->GetCurrentActorInfo();
+	if (!ActorInfo)
+	{
+		return false;
+	}
+
+	UAnimInstance* AnimInstance = ActorInfo->GetAnimInstance();
+	if (AnimInstance == nullptr)
+	{
+		return false;
+	}
+
+	// Check if the montage is still playing
+	// The ability would have been interrupted, in which case we should automatically stop the montage
+	if (AbilitySystemComponent && Ability)
+	{
+		if (AbilitySystemComponent->GetAnimatingAbility() == Ability
+			&& AbilitySystemComponent->GetCurrentMontage() == MontageToPlay)
+		{
+			// Unbind delegates so they don't get called as well
+			FAnimMontageInstance* MontageInstance = AnimInstance->GetActiveInstanceForMontage(MontageToPlay);
+			if (MontageInstance)
+			{
+				MontageInstance->OnMontageBlendingOutStarted.Unbind();
+				MontageInstance->OnMontageEnded.Unbind();
+			}
+
+			AbilitySystemComponent->CurrentMontageStop();
+			return true;
+		}
+	}
+
+	return false;
+}
+
+UDarkSoulAbilitySystemComponent* UDarkSoulAbilityTask_PlayMontage::GetTargetASC()
+{
+	return Cast<UDarkSoulAbilitySystemComponent>(AbilitySystemComponent);
+}
+
+void UDarkSoulAbilityTask_PlayMontage::OnMontageBlendingOut(UAnimMontage* Montage, bool bInterrupted)
+{
+	if (Ability && Ability->GetCurrentMontage() == MontageToPlay)
+	{
+		if (Montage == MontageToPlay)
+		{
+			AbilitySystemComponent->ClearAnimatingAbility(Ability);
+
+			// Reset AnimRootMotionTranslationScale
+			ACharacter* Character = Cast<ACharacter>(GetAvatarActor());
+			if (Character && (Character->GetLocalRole() == ROLE_Authority ||
+				(Character->GetLocalRole() == ROLE_AutonomousProxy && Ability->GetNetExecutionPolicy() == EGameplayAbilityNetExecutionPolicy::LocalPredicted)))
+			{
+				Character->SetAnimRootMotionTranslationScale(1.f);
+			}
+
+		}
+	}
+
+	if (bInterrupted)
+	{
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnInterrupted.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+	else
+	{
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnBlendOut.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+}
+
+void UDarkSoulAbilityTask_PlayMontage::OnAbilityCancelled()
+{
+	// TODO: Merge this fix back to engine, it was calling the wrong callback
+
+	if (StopPlayingMontage())
+	{
+		// Let the BP handle the interrupt as well
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnCancelled.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+}
+
+void UDarkSoulAbilityTask_PlayMontage::OnMontageEnded(UAnimMontage* Montage, bool bInterrupted)
+{
+	if (!bInterrupted)
+	{
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnCompleted.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+
+	EndTask();
+}
+
+void UDarkSoulAbilityTask_PlayMontage::OnGameplayEvent(FGameplayTag EventTag, const FGameplayEventData* Payload)
+{
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		FGameplayEventData TempData = *Payload;
+		TempData.EventTag = EventTag;
+
+		EventReceived.Broadcast(EventTag, TempData);
+	}
+}
