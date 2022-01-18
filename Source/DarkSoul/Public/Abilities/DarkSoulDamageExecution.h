471f88faa265e36ce71dfc8c956f85e60be148a9
yazj <yuanyi2000@protonmail.com>
2022-01-18 15:02:12 +0800

    Base Code

--- Source/DarkSoul/Public/Abilities/DarkSoulDamageExecution.h
+++ Source/DarkSoul/Public/Abilities/DarkSoulDamageExecution.h
@@ -1,17 +1,25 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Yazj, All Rights Reversed
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "DarkSoul.h"
 #include "GameplayEffectExecutionCalculation.h"
 #include "DarkSoulDamageExecution.generated.h"
 
 /**
- * 
+ * A damage execution, which allows doing damage by combining a raw Damage number with AttackPower and DefensePower
+ * Most games will want to implement multiple game-specific executions
  */
 UCLASS()
 class DARKSOUL_API UDarkSoulDamageExecution : public UGameplayEffectExecutionCalculation
 {
 	GENERATED_BODY()
 	
+public:
+	// Constructor and overrides
+	UDarkSoulDamageExecution();
+	virtual void Execute_Implementation(
+		const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+		OUT FGameplayEffectCustomExecutionOutput& OutExecutionOutput
+	) const override;
 };
