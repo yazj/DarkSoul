45da9f76e43a4c95527d383bdca660d5ebd6562c
yazj <yuanyi2000@protonmail.com>
2022-01-30 21:39:29 +0800

    LoadingScreen Module

--- Source/DarkSoulLoadingScreen/Public/DarkSoulLoadingScreen.h
+++ Source/DarkSoulLoadingScreen/Public/DarkSoulLoadingScreen.h
@@ -8,5 +8,15 @@
 /** Module interface for this game's loading screens */
 class IDarkSoulLoadingScreenModule : public IModuleInterface
 {
+public: 
+	static inline IDarkSoulLoadingScreenModule& Get()
+	{
+		return FModuleManager::LoadModuleChecked<IDarkSoulLoadingScreenModule>("DarkSoulLoadingScreen");
+	}
 
+	/** Kicks off the loading screen for in game loading (not startup) */
+	virtual void StartInGameLoadingScreen(bool bPlayUntilStopped, float PlayTime) = 0;
+
+	/** Stops the loading screen */
+	virtual void StopInGameLoadingScreen() = 0;
 };
